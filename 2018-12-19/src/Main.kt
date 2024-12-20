db7eea281b6eef27f911f19d4c4eb4e214df50f4
bernd <bernd@huskysoftware.de>
2024-12-20 02:54:22 +0100

    Solve 2018-12-19 finally

--- 2018-12-19/src/Main.kt
+++ 2018-12-19/src/Main.kt
@@ -15,7 +15,17 @@ fun main() {
         }
     }
 
+    disassemble(prg, ipBoundReg)
+
     println("Registers after running with reg[0] = 0: ${run(prg, ipBoundReg, 0).toList()}")
+    var sum = 0
+    for (i in 1..10551275) {
+        if (10551275 % i == 0) {
+            sum += i
+        }
+    }
+    println("Sum of prime factors of 875: $sum")
+
     println("Registers after running with reg[0] = 1: ${run(prg, ipBoundReg, 1).toList()}")
 }
 
@@ -51,4 +61,83 @@ fun run(prg: List<Instr>, ipBoundReg: Int, reg0StartVal: Int): IntArray {
         ip++
     }
     return reg
+}
+
+fun disassemble(prg: List<Instr>, ipBoundReg: Int) {
+    for (i in prg.indices) {
+        val (instr, a, b, c) = prg[i]
+
+        val humanReadale = if (c != ipBoundReg) {
+            when (instr) {
+                "addr" -> when {
+                    a == ipBoundReg -> "${reg2char(c)} = $i + ${reg2char(b)}"
+                    b == ipBoundReg -> "${reg2char(c)} = $i + ${reg2char(a)}"
+                    else -> "${reg2char(c)} = ${reg2char(a)} + ${reg2char(b)}"
+                }
+                "addi" -> "${reg2char(c)} = ${reg2char(a)} + $b"
+                "mulr" -> when {
+                    a == ipBoundReg -> "${reg2char(c)} = $i * ${reg2char(b)}"
+                    b == ipBoundReg -> "${reg2char(c)} = $i * ${reg2char(a)}"
+                    else -> "${reg2char(c)} = ${reg2char(a)} * ${reg2char(b)}"
+                }
+                "muli" -> "${reg2char(c)} = ${reg2char(a)} * $b"
+                "banr" -> "${reg2char(c)} = ${reg2char(a)} and ${reg2char(b)}"
+                "bani" -> "${reg2char(c)} = ${reg2char(a)} and $b"
+                "borr" -> "${reg2char(c)} = ${reg2char(a)} or ${reg2char(b)}"
+                "bori" -> "${reg2char(c)} = ${reg2char(a)} or $b"
+                "setr" -> when {
+                    a == ipBoundReg -> "${reg2char(c)} = $i"
+                    else -> "${reg2char(c)} = ${reg2char(a)}"
+                }
+                "seti" -> when {
+                    else -> "${reg2char(c)} = $a"
+                }
+                "gtir" -> "${reg2char(c)} = if ($a > ${reg2char(b)}) 1 else 0"
+                "gtri" -> "${reg2char(c)} = if (${reg2char(a)} > $b) 1 else 0"
+                "gtrr" -> "${reg2char(c)} = if (${reg2char(a)} > ${reg2char(b)}) 1 else 0"
+                "eqir" -> "${reg2char(c)} = if ($a == ${reg2char(b)}) 1 else 0"
+                "eqri" -> "${reg2char(c)} = if (${reg2char(a)} == $b) 1 else 0"
+                "eqrr" -> "${reg2char(c)} = if (${reg2char(a)} == ${reg2char(b)}) 1 else 0"
+                else -> {
+                    println("Mnemonic for $instr not known"); exitProcess(4)
+                }
+            }
+        } else {
+            when (instr) {
+                "addr" -> when {
+                    a == ipBoundReg -> "JMPREL ${reg2char(b)} + 1"
+                    b == ipBoundReg -> "JMPREL ${reg2char(a)} + 1"
+                    else -> "GOTO ${reg2char(a)} + ${reg2char(b)} + 1"
+                }
+                "addi" -> if (a == ipBoundReg) "GOTO ${i + b + 1}" else "GOTO ${reg2char(a)} + $b + 1"
+                "mulr" -> when {
+                    a == ipBoundReg && b == ipBoundReg -> "GOTO ${i * i + 1}"
+                    a == ipBoundReg -> "GOTO $i * ${reg2char(b)} + 1}"
+                    b == ipBoundReg -> "GOTO $i * ${reg2char(a)} + 1}"
+                    else -> "GOTO ${reg2char(a)} * ${reg2char(b)} + 1"
+                }
+                "muli" -> "GOTO ${reg2char(a)} * $b + 1"
+                "banr" -> "GOTO ${reg2char(a)} and ${reg2char(b)} + 1"
+                "bani" -> "GOTO ${reg2char(a)} and $b + 1"
+                "borr" -> "GOTO ${reg2char(a)} or ${reg2char(b)} + 1"
+                "bori" -> "GOTO ${reg2char(a)} or $b + 1"
+                "setr" -> "GOTO ${reg2char(a)} + 1"
+                "seti" -> "GOTO ${a + 1}"
+                "gtir" -> "GOTO if ($a > ${reg2char(b)}) 2 else 1"
+                "gtri" -> "GOTO if (${reg2char(a)} > $b) 2 else 1"
+                "gtrr" -> "GOTO if (${reg2char(a)} > ${reg2char(b)}) 2 else 1"
+                "eqir" -> "GOTO if ($a == ${reg2char(b)}) 2 else 1"
+                "eqri" -> "GOTO if (${reg2char(a)} == $b) 2 else 1"
+                "eqrr" -> "GOTO if (${reg2char(a)} == ${reg2char(b)}) 2 else 1"
+                else -> {
+                    println("Mnemonic for $instr not known"); exitProcess(4)
+                }
+            }
+        }
+        println("$i: $humanReadale")
+    }
+}
+
+fun reg2char(reg: Int): Char {
+    return ('A'.code + reg).toChar()
 }
